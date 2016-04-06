55b288f33a2b80bd2163d6403c731075be62f7e3
Palmer Harrell <palmerharrell@me.com>
2016-04-06 14:05:06 -0500

    Version 2 exercise complete

--- animal.cs
+++ animal.cs
@@ -1,17 +1,33 @@
 
-close
 class Animal {
     public string AnimalName { get; set; }
 }
+
 class Species : Animal {
     public string SpeciesName { get; set; }
 }
+
 class Squirrel: Species {
     public string Noise {get; set;}
     public bool Exotic {get; set;}
     public string FavoriteFood {get; set;}
     public bool FluffyTail {get; set;}
     public int CutenessLevel {get; set;}
+    public virtual string Transportation() {
+    return "scurries";
+  }
 }
+
+class FlyingSquirrel : Squirrel {
+  public bool FearOfCars {get; set;}
+  public override string Transportation() {
+    return "flies";
+  }
+}
+
 Squirrel nutsy = new Squirrel();
-Console.Write("I have a Squirrel named Nutsy who makes a noise that sounds like {0}. I'd rate  his cuteness as a {1}.", nutsy.Noise, nutsy.CutenessLevel);
+FlyingSquirrel batman = new FlyingSquirrel();
+
+Console.WriteLine(nutsy.Transportation());
+Console.WriteLine(batman.Transportation());
+
