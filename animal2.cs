633450723274adb045d55a5d98307dd5efd3caee
Palmer Harrell <palmerharrell@me.com>
2016-04-07 10:03:16 -0500

    Created all species under Equus genus

--- animal2.cs
+++ animal2.cs
@@ -6,6 +6,11 @@ class Animal {
 class Equus : Animal {
 	private string _genus = "Equus";
 	private int _legs = 4;
+	private bool _isHybrid;
+	public bool isHybrid {
+		get { return _isHybrid; }
+		set { _isHybrid = value; }
+	}
 	public string genus {
     get { return _genus; }
   }
@@ -15,13 +20,10 @@ class Equus : Animal {
 }
 
 
-// SPECIES
+// SPECIES (Genus: Equus)
 class Horse : Equus {
+	public string speciesName = "Horse";
 	private bool _isHybrid = false;
-	public bool isHybrid {
-		get { return _isHybrid; }
-		set { _isHybrid = value; }
-	}
 	// CONSTRUCTOR
 	public Horse (string animalName) {
 		name = animalName;
@@ -30,11 +32,8 @@ class Horse : Equus {
 }
 
 class Zebra : Equus {
+	public string speciesName = "Zebra";
 	private bool _isHybrid = false;
-	public bool isHybrid {
-		get { return _isHybrid; }
-		set { _isHybrid = value; }
-	}
 	// CONSTRUCTOR
 	public Zebra (string animalName) {
 		name = animalName;
@@ -42,20 +41,82 @@ class Zebra : Equus {
 	}
 }
 
+class Donkey : Equus {
+	public string speciesName = "Donkey";
+	private bool _isHybrid = false;
+	// CONSTRUCTOR
+	public Donkey (string animalName) {
+		name = animalName;
+		isHybrid = false;
+	}
+}
+
+class Mule : Equus {
+	public string speciesName = "Mule";
+	private bool _isHybrid = true;
+	// CONSTRUCTOR
+	public Mule (string animalName) {
+		name = animalName;
+		isHybrid = true;
+	}
+}
+
+class Zedonk : Equus {
+	public string speciesName = "Zedonk";
+	private bool _isHybrid = true;
+	// CONSTRUCTOR
+	public Zedonk (string animalName) {
+		name = animalName;
+		isHybrid = true;
+	}
+}
 
 // Create animal instances
 Horse mrEd = new Horse("Mr. Ed");
 Zebra stripey = new Zebra("Stripey");
+Donkey eeyore = new Donkey("Eeyore");
+Mule stumpy = new Mule("Stumpy");
+Zedonk zippy = new Zedonk("Zippy");
 
 // Output
+Console.WriteLine("");
+
 Console.WriteLine(mrEd.name);
 Console.WriteLine(mrEd.genus);
+Console.WriteLine(mrEd.speciesName);
 Console.WriteLine(mrEd.legs);
 Console.WriteLine(mrEd.isHybrid);
+Console.WriteLine("");
 
 Console.WriteLine(stripey.name);
 Console.WriteLine(stripey.genus);
+Console.WriteLine(stripey.speciesName);
 Console.WriteLine(stripey.legs);
 Console.WriteLine(stripey.isHybrid);
+Console.WriteLine("");
+
+Console.WriteLine(eeyore.name);
+Console.WriteLine(eeyore.genus);
+Console.WriteLine(eeyore.speciesName);
+Console.WriteLine(eeyore.speciesName);
+Console.WriteLine(eeyore.legs);
+Console.WriteLine(eeyore.isHybrid);
+Console.WriteLine("");
+
+Console.WriteLine(stumpy.name);
+Console.WriteLine(stumpy.genus);
+Console.WriteLine(stumpy.speciesName);
+Console.WriteLine(stumpy.legs);
+Console.WriteLine(stumpy.isHybrid);
+Console.WriteLine("");
+
+Console.WriteLine(zippy.name);
+Console.WriteLine(zippy.genus);
+Console.WriteLine(zippy.speciesName);
+Console.WriteLine(zippy.legs);
+Console.WriteLine(zippy.isHybrid);
+Console.WriteLine("");
+
+
 
 
