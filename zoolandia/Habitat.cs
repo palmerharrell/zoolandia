8c62290d14ef926dd75bb7c124474276b2260cd4
palmerharrell <palmerharrell@PALMERHARREEC76>
2016-04-12 19:24:01 -0500

    Fixing indentation

--- zoolandia/Habitat.cs
+++ zoolandia/Habitat.cs
@@ -8,16 +8,14 @@ namespace zoolandia
 {
   class Habitat
   {
-    public bool hasWalls;
-    public bool outside;
-    public string climateType;
-    public bool isAquatic;
-    public double baseTemperature;
-    public double width;
-    public double height;
-    public double depth;
-    public int windowCount;
-
+    public bool hasWalls = true;
+    public string climateType = "";
+    public bool isAquatic = false;
+    public double baseTemperature = 0;
+    public double width = 0;
+    public double depth = 0;
+    public double height = 0;
+    public int windowCount = 0;
     public List<Animal> inhabitants;
   }
 }
--- zoolandia/Program.cs
+++ zoolandia/Program.cs
@@ -8,57 +8,29 @@ using zoolandia.Species;
 namespace zoolandia
 {
     
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Habitat savanna = new Habitat();
-            savanna.inhabitants = new List<Animal>();
+      Habitat savanna = new Habitat();
+      savanna.inhabitants = new List<Animal>();
         
-            // Animal larry = new Animal();
-            // larry.name = args[0];
+      // Animal larry = new Animal();
+      // larry.name = args[0];
 
-            // ANIMAL INSTANCES
-            Zedonk zippy = new Zedonk("Zippy");
-            Latastei marco = new Latastei("Marco");
-            Iberica robert = new Iberica("Robert");
+      // ANIMAL INSTANCES
+      Zedonk zippy = new Zedonk("Zippy");
+      Latastei marco = new Latastei("Marco");
+      Iberica robert = new Iberica("Robert");
 
-            savanna.inhabitants.Add(zippy);
-            savanna.inhabitants.Add(marco);
+      savanna.inhabitants.Add(zippy);
+      savanna.inhabitants.Add(marco);
 
-            foreach (Animal a in savanna.inhabitants)
-            {
-              Console.WriteLine("List and foreach example: " + a.name);
-            }
-
-            // CONSOLE OUTPUT
-            Console.WriteLine("Name: " + zippy.name);
-            Console.WriteLine("Genus: " + zippy.genus);
-            Console.WriteLine("Species: " + zippy.speciesName);
-            Console.WriteLine("Common Name: " + zippy.commonName);
-            Console.WriteLine("Legs: " + zippy.legs);
-            Console.WriteLine("Hybrid: " + zippy.isHybrid);
-            Console.WriteLine("Fertile: " + zippy.isFertile);
-            Console.WriteLine("");
-
-            Console.WriteLine("Name: " + marco.name);
-            Console.WriteLine("Genus: " + marco.genus);
-            Console.WriteLine("Species: " + marco.speciesName);
-            Console.WriteLine("Common Name: " + marco.commonName);
-            Console.WriteLine("Legs: " + marco.legs);
-            Console.WriteLine("Color: " + marco.color);
-            Console.WriteLine("Override test: " + marco.doSomething());
-            Console.WriteLine("");
-
-            Console.WriteLine("Name: " + robert.name);
-            Console.WriteLine("Genus: " + robert.genus);
-            Console.WriteLine("Species: " + robert.speciesName);
-            Console.WriteLine("Common Name: " + robert.commonName);
-            Console.WriteLine("Legs: " + robert.legs);
-            Console.WriteLine("Color: " + robert.color);
-            Console.WriteLine("Override test: " + robert.doSomething());
-            Console.WriteLine("");
-
-        }
+      // CONSOLE OUTPUT
+      foreach (Animal a in savanna.inhabitants)
+      {
+        Console.WriteLine("List and foreach example: " + a.name);
+      }
     }
+  }
 }
